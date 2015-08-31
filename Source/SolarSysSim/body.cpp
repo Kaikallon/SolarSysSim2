51fc2ab4f88fb79fc09daa162d57e2bae67d77a6
Kaikallon <axelander2_lol@hotmail.com>
2015-09-01 00:44:38 +0200

    Initial Abody

--- Source/SolarSysSim/body.cpp
+++ Source/SolarSysSim/body.cpp
@@ -8,8 +8,25 @@
 Abody::Abody()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-	int test = 1;
+	PrimaryActorTick.bCanEverTick = false;
+	this->gConst = 1;
+	this->mass = 1;
+	this->dT = 1;
+	this->vel = { 0, 0, 0 };
+	this->pos = { 0, 0, 0 };
+
+
+}
+Abody::Abody(FVector vel, FVector pos, float mass, float radius)
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	this->gConst = 1;
+	this->mass = mass;
+	this->dT = 1;
+	this->vel = vel;
+	this->pos = pos;
 
 }
 
@@ -27,3 +44,33 @@ void Abody::Tick( float DeltaTime )
 
 }
 
+void Abody::CalcForces(Abody* planB)
+{
+	//Reset all forces
+	this->Forces.X = 0;
+	this->Forces.Y = 0;
+	this->Forces.Z = 0;
+
+	float dist = this->GetDistanceTo(planB);
+	float AbsForce = (gConst * mass * planB->mass)/ pow(dist, 2);
+	//CalcForces
+	this->Forces.X += (GetActorLocation().X - planB->GetActorLocation().X) / dist;
+	this->Forces.Y += (GetActorLocation().Y - planB->GetActorLocation().Y) / dist;
+	this->Forces.Z += (GetActorLocation().Z - planB->GetActorLocation().Z) / dist;
+
+}
+
+void Abody::CalcAcc()
+{
+	acc.X = Forces.X / mass; 
+	acc.Y = Forces.Y / mass;
+	acc.Z = Forces.Z / mass;
+}
+void Abody::CalcVel()
+{
+	vel += acc; // * dT;
+}
+void Abody::CalcPos()
+{
+	pos += acc; // *dT;
+}
--- Source/SolarSysSim/body.h
+++ Source/SolarSysSim/body.h
@@ -5,6 +5,7 @@
 #include "GameFramework/Actor.h"
 #include "body.generated.h"
 
+
 UCLASS()
 class SOLARSYSSIM_API Abody : public AActor
 {
@@ -12,7 +13,8 @@ class SOLARSYSSIM_API Abody : public AActor
 	
 public:	
 	// Sets default values for this actor's properties
-	Abody();
+	Abody(FVector vel, FVector pos, float mass, float radius);
+
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -20,6 +22,19 @@ public:
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
+	//Metoder
+
+	void CalcForces(Abody* planB);
+	void CalcAcc();
+	void CalcVel();
+	void CalcPos();
 	
-	
+private:
+	Abody();
+
+	//Variabler
+	FVector Forces, acc, vel, pos;
+	float mass, dT, radius;
+	float gConst;  //Make static
+
 };
